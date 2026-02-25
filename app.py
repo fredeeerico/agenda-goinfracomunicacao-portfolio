592d6f35b0ad87a8ee3580fb74635e2bb25816da
Fred Augusto <fredeeerico@icloud.com>
2026-02-25 09:53:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import psycopg2
 import psycopg2.extras
+pip install gspread google-auth
 from datetime import date, time, datetime, timedelta, timezone
 
 # ======================================================
@@ -413,3 +414,4 @@ elif st.session_state.aba_atual == "LISTA":
             )
             conn.commit()
             st.rerun()
+
